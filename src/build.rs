2c85553bf4f7f2e8a20924b58318258d87878ef4
DOTDIGITALGROUP\grant.lodge <grant.lodge@dotmailer.com>
2017-03-15 10:09:54 +0000

    start to set up messages

--- src/build.rs
+++ src/build.rs
@@ -3,11 +3,13 @@ use std::path::Path;
 pub fn run<P: AsRef<Path>>(root: P) {
     let root = root.as_ref();
 
-    let folders = vec!["schema"];
+    let folders = vec!["filegroup","partition", "schema", "table"];
 
     for folder in folders {
         let path = root.join(folder);
-
-        print!("{}", path.display());
+        print!("Deploying {}s readinging from {}", folder, path);
+    
+        
+        print!("Successfully deployed {}s in {}", folder, path);
     }
 }
