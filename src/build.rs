8760e7b94fe791de3b2358f6f27b05c1a1b20291
DOTDIGITALGROUP\grant.lodge <grant.lodge@dotmailer.com>
2017-03-15 13:22:58 +0000

    start dir work

--- src/build.rs
+++ src/build.rs
@@ -1,4 +1,7 @@
+use std::io;
+use std::fs::{self, DirEntry};
 use std::path::Path;
+
 
 pub fn run<P: AsRef<Path>>(root: P) {
     let root = root.as_ref();
@@ -7,9 +10,16 @@ pub fn run<P: AsRef<Path>>(root: P) {
 
     for folder in folders {
         let path = root.join(folder);
-        print!("Deploying {}s readinging from {}", folder, path);
-    
+        print!("Deploying {}s readinging from {}", folder, path.display());
         
-        print!("Successfully deployed {}s in {}", folder, path);
+        if !path.is_dir() {
+            panic!("{} could not be located", path.display());
+        }
+                                           
+        for file in try!(fs::read_dir(path)) {
+            
+        }
+        
+        print!("Successfully deployed {}s in {}", folder, path.display());
     }
 }
