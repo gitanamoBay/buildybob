80541573fff67fd776857777617be8038d5fe6d2
Grant.Lodge <grant.lodge@dotmailer.com>
2017-10-31 08:09:22 +0000

    config files

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,14 @@ const USAGE: &'static str = "
 buildybob deploys database.
 
 Usage:
-    buildybob <path> [--clean | --configfile=<str>]
+    buildybob <path> [--clean | --configfile=<str> | --export=<str>]
+    buildybob <configfile>
     
 Options:
     -h --help               Show this screen.
     --version               Show Version.
     --clean                 Deploy fresh copy of database.
+    --export=<str>          Exports config to name
     --configfile=<str>      Which meta file to use at root      
 ";
 
@@ -25,8 +27,16 @@ const DEFAULT_SERVER: &'static str = "postgresql://postgres@localhost";
 #[derive(Debug, RustcDecodable)]
 struct Args {
     flag_clean: bool,
-    flag_meta: String,
+    arg_configfile: String,
     arg_path: String,
+}
+
+
+struct ConfigFile {
+    server: String,
+    db_name: String,
+    clean: bool,
+    port: i32
 }
 
 fn main() {
@@ -36,6 +46,10 @@ fn main() {
 
     if !args.flag_server.is_empty() {
         server = args.flag_server.to_string();
+    }
+    
+    if !args.flag_configfile.is_empty() {
+
     }
 
     build::run(args.arg_path, &server);
